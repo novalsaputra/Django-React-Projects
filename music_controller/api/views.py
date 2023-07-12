93672622d7009d4a65bd20c1f5c1f97f78709f24
noval.saputra <noval.saputra@bitcoin.co.id>
2023-07-13 02:14:46 +0700

    configure frontend react in django

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -4,12 +4,12 @@ from .serializers import RoomSerializer
 from .models import Room
 
 # from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
-def main(request):
-    return HttpResponse("<h1>Hello Bro</h1>")
+# def main(request):
+#     return HttpResponse("<h1>Hello Bro</h1>")
 
 
 class RoomView(generics.ListAPIView): #using CreateAPIView for post api
